2359882f113363c4f1be1187901def5a6976d325
Drew Harris <dsharris10@gmail.com>
2022-04-06 22:23:46 -0500

    Multithreading superjson reads from folder

--- main.cpp
+++ main.cpp
@@ -1,27 +1,83 @@
 #include <iostream>
 #include <fstream>
+#include <queue>
+#include <filesystem>
+#include <dirent.h>
+#include <mutex>
+#include <thread>
 #include "include/rapidjson/document.h"
+#include <chrono>
+
+std::mutex queue_mutex;
+std::queue<std::string> filenames;
+
+void processFiles(std::string path) {
+    while(!filenames.empty()) {
+        queue_mutex.lock();
+        std::string filename = filenames.front();
+        filenames.pop();
+        queue_mutex.unlock();
+
+        std::ifstream file(path + filename);
+        if (!file.is_open()) {
+            std::cout << "Could not open file: " << filename << std::endl;
+            continue;
+        }
+        rapidjson::Document document;
+        try {
+
+            std::string content((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
+            document.Parse(content.c_str());
+        } catch (std::exception& e) {
+            std::cout << "Could not read file: " << filename << std::endl;
+            continue;
+        }
+        file.close();
+
+
+        assert(document.IsObject());
+        assert(document.HasMember("title"));
+        assert(document["title"].IsString());
+    }
+}
 
 int main() {
-    std::string path = "../data/articles/2018_01_112b52537b67659ad3609a234388c50a/news_0037810.json";
+    std::string path = "../data/articles/2018_01_112b52537b67659ad3609a234388c50a/";
 
-    std::ifstream ifs(path);
-    if (!ifs.is_open()) {
-        std::cout << "open file failed" << std::endl;
-        return -1;
+    if (auto dir = opendir(path.c_str())) {
+        while (auto f = readdir(dir)) {
+            // if begins with dots, skip
+            if (f->d_name[0] == '.') {
+                continue;
+            }
+            filenames.push(f->d_name);
+        }
+        closedir(dir);
     }
-    std::string json((std::istreambuf_iterator<char>(ifs)),
-                     (std::istreambuf_iterator<char>()));
-    rapidjson::Document document;
 
-    document.Parse(json.c_str());
 
-    assert(document.IsObject());
-    assert(document["title"].IsString());
-    assert(document["uuid"].IsString());
+    std::thread t1(processFiles, path);
+    std::thread t2(processFiles, path);
+    std::thread t3(processFiles, path);
 
-    std::cout << "Title: " << document["title"].GetString() << std::endl;
-    std::cout << "UUID: " << document["uuid"].GetString() << std::endl;
+    using std::chrono::high_resolution_clock;
+    using std::chrono::duration_cast;
+    using std::chrono::duration;
+    using std::chrono::milliseconds;
+
+    auto timer1 = high_resolution_clock::now();
+    t1.join();
+    t2.join();
+    t3.join();
+    auto timer2 = high_resolution_clock::now();
+
+    /* Getting number of milliseconds as an integer. */
+    auto ms_int = duration_cast<milliseconds>(timer2 - timer1);
+
+    /* Getting number of milliseconds as a double. */
+    duration<double, std::milli> ms_double = timer2 - timer1;
+
+    std::cout << ms_int.count() << "ms\n";
 
 
 

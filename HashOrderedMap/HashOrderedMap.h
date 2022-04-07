c5c4d5d7bf2deabef3ede7861da1ef269bcc1c85
adamcesco <acescobedo@smu.edu>
2022-04-07 03:39:18 -0500

    defined some basic methods for HashOrderedMap class. Created an in-class struct HashPair. Re-wrote templated code that contained some compile-errors. Defined the following methods for HashOrderedMap:     HashOrderedMap()     int size() const     bool is_empty() const     HashOrderedMap<T, U>& emplace_pair(const T&, const U&)     U& operator[](const T&)     U read_at(const T&) const     ~HashOrderedMap()

--- HashOrderedMap/HashOrderedMap.h
+++ HashOrderedMap/HashOrderedMap.h
@@ -1,27 +1,160 @@
 //
-// Created by misc1 on 4/7/2022.
+// Created by Adam Escobedo on 4/7/2022.
 //
 
 #ifndef INC_22S_FINAL_PROJ_HASHORDEREDMAP_H
 #define INC_22S_FINAL_PROJ_HASHORDEREDMAP_H
 
+#include <functional>
+#include <string>
+
 template<class T, class U>
 class HashOrderedMap{
 public:
     HashOrderedMap();
-    HashOrderedMap(const HashOrderedMap&);
-    HashOrderedMap& operator=(const HashOrderedMap&);
-    HashOrderedMap& clear();
-    HashOrderedMap& erase_at(const T&);
-    int size();
-    bool is_empty();
-    HashOrderedMap& emplace_pair(const T&, const U&);
+    HashOrderedMap(const HashOrderedMap<T, U>&);
+    HashOrderedMap<T, U>& operator=(const HashOrderedMap<T, U>&);
+    HashOrderedMap<T, U>& clear() { delete[] data; data = new HashPair[20]; max_cap = 20; return *this; }
+    HashOrderedMap<T, U>& clear_value_at(const T&);
+    int size() const { return ele_count; }
+    bool is_empty() const { return ele_count == 0; }
+    HashOrderedMap<T, U>& emplace_pair(const T&, const U&);
     U& operator[](const T&);
-    U read_at(const T&);
-    ~HashOrderedMap();
+    U read_at(const T&) const;
+    ~HashOrderedMap() { delete[] data; }
 private:
-    U* data;
-    int ele_count;
+    explicit HashOrderedMap(int);
+    unsigned int increase_max_cap(const T &key);
+
+    struct HashPair{
+        HashPair()=default;
+        HashPair(unsigned int hashNum, const U& val) { hash = hashNum; value = new U(val); }
+        HashPair(const HashPair& toCopy) { hash = toCopy.hash; if(toCopy.value != nullptr){ value = new U(*toCopy.value); } }
+        HashPair& operator=(const HashPair& toAssign) {
+            if(this == &toAssign){ return *this; }
+            hash = toAssign.hash; delete value; value = nullptr;
+            if(toAssign.value != nullptr){ value = new U(*toAssign.value); }
+            return *this; }
+        ~HashPair() { delete value; }
+
+        unsigned int hash = 0;
+        U* value = nullptr;
+    };
+
+    HashPair* data = nullptr;
+    int ele_count = 0;
+    int max_cap;
 };
+
+template<class T, class U>
+HashOrderedMap<T, U>::HashOrderedMap() {
+    data = new HashPair[20];
+    max_cap = 20;
+}
+
+template<class T, class U>
+HashOrderedMap<T, U>& HashOrderedMap<T, U>::emplace_pair(const T &key, const U &value) {
+    std::hash<T> hashObj;
+    const unsigned int index = hashObj(key);
+    int index_clean = index % max_cap;
+    HashPair toInsert(index, value);
+
+    while(data[index_clean].value != nullptr && index_clean < max_cap){
+        if(data[index_clean].hash == index) {
+            --ele_count;
+            break;
+        }
+        ++index_clean;
+    }
+
+    if(index_clean >= max_cap) {
+        index_clean = increase_max_cap(key);
+    }
+
+    data[index_clean] = toInsert;
+    ++ele_count;
+
+    return *this;
+}
+
+template<class T, class U>
+U& HashOrderedMap<T, U>::operator[](const T &key) {
+    std::hash<T> hashObj;
+    const unsigned int index = hashObj(key);
+    int index_clean = index % max_cap;
+    while(data[index_clean].value != nullptr && data[index_clean].hash != index && index_clean < max_cap){
+        ++index_clean;
+    }
+
+    if(index_clean >= max_cap) {
+        index_clean = increase_max_cap(key);
+    }
+
+    data[index_clean].hash = index;
+
+    if(data[index_clean].value == nullptr) {
+        data[index_clean].value = new U();
+        ++ele_count;
+    }
+
+    return *data[index_clean].value;
+}
+
+template<class T, class U>
+U HashOrderedMap<T, U>::read_at(const T &key) const {
+    std::hash<T> hashObj;
+    unsigned int index = hashObj(key);
+    int index_clean = index % max_cap;
+    while((data[index_clean].value == nullptr || data[index_clean].hash != index) && index_clean < max_cap){
+        ++index_clean;
+    }
+
+    if(index_clean >= max_cap) {
+        throw std::invalid_argument("Error in U HashOrderedMap<T, U>::read_at(const T &key) const | key not found");
+    }
+
+    return *data[index_clean].value;
+}
+
+template<class T, class U>
+unsigned int HashOrderedMap<T, U>::increase_max_cap(const T &key) {
+    int mcCopy = max_cap;
+    max_cap *= 2;
+    auto* dataCopy = new HashPair[max_cap];
+    for (int i = 0; i < mcCopy; ++i) {
+        if(data[i].value == nullptr)
+            continue;
+        const unsigned int index = data[i].hash;
+        int index_clean = index % max_cap;
+        HashPair toInsert(index, *data[i].value);
+
+        while(dataCopy[index_clean].value != nullptr && index_clean < max_cap){
+            if(dataCopy[index_clean].hash == index) {
+                break;
+            }
+            ++index_clean;
+        }
+        dataCopy[index_clean] = toInsert;
+    }
+    delete[] data;
+    data = dataCopy;
+
+    std::hash<T> hashObj;
+    const unsigned int index = hashObj(key);
+    int index_clean = index % max_cap;
+    while(data[index_clean].value != nullptr && index_clean < max_cap){
+        if(data[index_clean].hash == index) {
+            break;
+        }
+        ++index_clean;
+    }
+    return index_clean;
+}
+
+template<class T, class U>
+HashOrderedMap<T, U>::HashOrderedMap(int max_req) {
+    data = new HashPair[max_req];
+    max_cap = max_req;
+}
 
 #endif //INC_22S_FINAL_PROJ_HASHORDEREDMAP_H
---
+++ HashOrderedMap/HashOrderedMap_tests.cpp
@@ -0,0 +1,59 @@
+//
+// Created by Adam Escobedo on 4/7/2022.
+//
+
+#include "../catch.hpp"
+#include "HashOrderedMap.h"
+#include <iostream>
+
+TEST_CASE("Testing basic constructors and destructor", "[HashOrderedMap]"){
+    SECTION("Testing default constructor"){
+        HashOrderedMap<char, int> testDummy;
+        REQUIRE(testDummy.size() == 0);
+        REQUIRE(testDummy.is_empty() == true);
+    }
+}
+
+TEST_CASE("Testing HashOrderedMap filling", "[HashOrderedMap]"){
+    SECTION("Testing \"U& HashOrderedMap<T, U>::operator[](const T&)\""){
+        HashOrderedMap<char, int> testDummy;
+        testDummy['A'] = 123;
+        REQUIRE(testDummy['A'] == 123);
+        REQUIRE(testDummy.size() == 1);
+        REQUIRE(testDummy.is_empty() == false);
+
+        HashOrderedMap<std::string, int> homTestString;
+        homTestString["Adam"] = 123;
+        REQUIRE(homTestString["Adam"] == 123);
+        REQUIRE(homTestString.size() == 1);
+        REQUIRE(homTestString.is_empty() == false);
+    }
+
+    SECTION("Testing \"U HashOrderedMap<T, U>::read_at(const T &key) const\""){
+        HashOrderedMap<char, int> testDummy;
+        testDummy['A'] = 123;
+        REQUIRE(testDummy.read_at('A') == 123);
+    }
+
+    SECTION("Testing \"void HashOrderedMap<T, U>::increase_max_cap()\""){
+        HashOrderedMap<char, int> testDummy;
+        int counter[27];
+        for (int & it : counter) {
+            it = 0;
+        }
+
+        for (int i = 0; i < 100; ++i) {
+            char key = (i % 26) + 'a';
+            testDummy[key] += i;
+            counter[key & 31] += i;
+        }
+        REQUIRE(testDummy.size() == 26);
+        REQUIRE(testDummy.is_empty() == false);
+
+        for (int i = 0; i < 26; ++i) {
+            char key = i + 'a';
+            REQUIRE(testDummy[key] == counter[key & 31]);
+            REQUIRE(testDummy.read_at(key) == counter[key & 31]);
+        }
+    }
+}

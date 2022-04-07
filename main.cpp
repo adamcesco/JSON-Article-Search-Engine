e23972ec913926196e92a27d55e654a6ee502c4c
Drew Harris <dsharris10@gmail.com>
2022-04-06 21:52:29 -0500

    Parse single json file (test) (main.cpp)

--- main.cpp
+++ main.cpp
@@ -1,6 +1,29 @@
 #include <iostream>
+#include <fstream>
+#include "include/rapidjson/document.h"
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::string path = "../data/articles/2018_01_112b52537b67659ad3609a234388c50a/news_0037810.json";
+
+    std::ifstream ifs(path);
+    if (!ifs.is_open()) {
+        std::cout << "open file failed" << std::endl;
+        return -1;
+    }
+    std::string json((std::istreambuf_iterator<char>(ifs)),
+                     (std::istreambuf_iterator<char>()));
+    rapidjson::Document document;
+
+    document.Parse(json.c_str());
+
+    assert(document.IsObject());
+    assert(document["title"].IsString());
+    assert(document["uuid"].IsString());
+
+    std::cout << "Title: " << document["title"].GetString() << std::endl;
+    std::cout << "UUID: " << document["uuid"].GetString() << std::endl;
+
+
+
     return 0;
 }

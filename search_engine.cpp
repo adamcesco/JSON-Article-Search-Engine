03c031859d4a63b2c509aabf70a4e8cfa2306fa2
Drew Harris <61394759+drew-harris@users.noreply.github.com>
2022-04-08 01:09:55 -0500

    Awesome progress bar

--- search_engine.cpp
+++ search_engine.cpp
@@ -18,16 +18,28 @@ SearchEngine::~SearchEngine() {
     delete this->authors;
 }
 
+void printProgressBar(double progress) {
+    int barWidth = 70;
+    std::cout << "[";
+    int pos = barWidth * progress;
+    for (int i = 0; i < barWidth; ++i) {
+        if (i < pos) std::cout << "■";
+        else if (i == pos) std::cout << "▶";
+        else std::cout << " ";
+    }
+    std::cout << "] " << int(progress * 100.0) << " %\r";
+    std::cout.flush();
+}
+
 void SearchEngine::generateIndex() {
     std::future<std::string> fut = std::async(std::launch::async, &Processor::generateIndex, this->processor,
                                               this->data_folder);
     while (fut.wait_for(std::chrono::milliseconds(41)) != std::future_status::ready) {
         double progress = this->processor->getProgress();
         if (progress > 0) {
-            printf("\rAny text done\r%7.2f%%", processor->getProgress() * 100);
-            fflush(stdout); // <- add this call
+            printProgressBar(progress);
         }
     }
-    printf("\rAny text done\r%7.2f%%", processor->getProgress() * 100);
+    printProgressBar(1);
     std::cout << std::endl;
 }

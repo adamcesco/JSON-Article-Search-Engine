0f1c45e30d63148af3740f14c9cb8a9d7fbdf308
adamcesco <acescobedo@smu.edu>
2022-04-12 21:43:05 -0500

    added stemming for the searched word

--- search_engine.cpp
+++ search_engine.cpp
@@ -9,6 +9,7 @@
 #include "./include/termcolor/termcolor.hpp"
 #include "./TableBundle.h"
 #include <iomanip>      // std::setprecision
+#include "./include/porter2_stemmer/porter2_stemmer.h"
 
 
 SearchEngine::SearchEngine(std::string data_folder) {
@@ -78,9 +79,13 @@ void SearchEngine::generateIndex() {
 
 void SearchEngine::testFindWord(std::string word) {
 //    this->wordTree->print_tree_inorder();
+    Porter2Stemmer::stem(word);
     tbb::concurrent_vector<std::string *> *result = this->wordTree->get_at(word);
     std::cout << "Found " << result->size() << " articles containing the word " << word << ":" << std::endl;
+    std::string *prev;
 //    for (std::string *article: *result) {
-//        std::cout << *article << std::endl;
+//        if (prev != article)
+//            std::cout << *article << std::endl;
+//        prev = article;
 //    }
 }

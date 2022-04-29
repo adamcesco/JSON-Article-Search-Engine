d367786ac4ecfe236ddd0f1cabf89eb972f5f324
adamcesco <acescobedo@smu.edu>
2022-04-28 21:35:49 -0500

    fixed build multiple definition error

---
+++ utils.cpp
@@ -0,0 +1,63 @@
+//
+// Created by misc1 on 4/28/2022.
+//
+
+#include "utils.h"
+
+std::string getCenteredText(const std::string &text, int width) {
+    int textLength = text.length();
+    int spaces = (width - textLength) / 2;
+    std::string spacesStr = "";
+    for (int i = 0; i < spaces; i++) {
+        spacesStr += " ";
+    }
+    return spacesStr + text;
+}
+
+void toLower(std::string &str) {
+    for (char &i: str) {
+        i = tolower(i);
+    }
+}
+
+void removePunctuation(std::string &str) {
+    std::string clean;
+    for (auto &it: str) {
+        if (std::isalpha(it)) {
+            clean += it;
+        }
+    }
+    str = clean;
+}
+
+void cleanStr(std::string &str) {
+    std::string clean;
+    for (char &it: str) {
+        if (std::isalpha(it)) {
+            clean += tolower(it);
+        }
+    }
+    str = clean;
+}
+
+std::string cleanPropnoun(const std::string &str) {
+    std::string clean;
+    for (const char &it: str) {
+        if (std::isalpha(it)) {
+            clean += tolower(it);
+        } else if (it == ' ') {
+            clean += '-';
+        }
+    }
+    return clean;
+}
+
+unsigned int custom_string_hash(const std::string &str) {
+    unsigned int hashed = 1;
+    for (const char &cc: str)
+        if (std::isalpha(cc)) {
+            hashed *= 16777619;
+            hashed = hashed ^ (cc & 31);
+        }
+    return hashed;
+}
--- utils.h
+++ utils.h
@@ -8,62 +8,16 @@
 #include <string>
 #include "./include/porter2_stemmer/porter2_stemmer.h"
 
-std::string getCenteredText(const std::string &text, int width) {
-    int textLength = text.length();
-    int spaces = (width - textLength) / 2;
-    std::string spacesStr = "";
-    for (int i = 0; i < spaces; i++) {
-        spacesStr += " ";
-    }
-    return spacesStr + text;
-}
+std::string getCenteredText(const std::string &text, int width);
 
-void toLower(std::string &str) {
-    for (char &i: str) {
-        i = tolower(i);
-    }
-}
+void toLower(std::string &str);
 
-void removePunctuation(std::string &str) {
-    std::string clean;
-    for (auto &it: str) {
-        if (std::isalpha(it)) {
-            clean += it;
-        }
-    }
-    str = clean;
-}
+void removePunctuation(std::string &str);
 
-void cleanStr(std::string &str) {
-    std::string clean;
-    for (char &it: str) {
-        if (std::isalpha(it)) {
-            clean += tolower(it);
-        }
-    }
-    str = clean;
-}
+void cleanStr(std::string &str);
 
-std::string cleanPropnoun(const std::string &str) {
-    std::string clean;
-    for (const char &it: str) {
-        if (std::isalpha(it)) {
-            clean += tolower(it);
-        } else if (it == ' ') {
-            clean += '-';
-        }
-    }
-    return clean;
-}
+std::string cleanPropnoun(const std::string &str);
 
-unsigned int custom_string_hash(const std::string &str) {
-    unsigned int hashed = 1;
-    for (const char &cc: str)
-        if (std::isalpha(cc)) {
-            hashed *= 16777619;
-            hashed = hashed ^ (cc & 31);
-        }
-    return hashed;
-}
+unsigned int custom_string_hash(const std::string &str);
 
 #endif //INC_22S_FINAL_PROJ_UTILS_H
